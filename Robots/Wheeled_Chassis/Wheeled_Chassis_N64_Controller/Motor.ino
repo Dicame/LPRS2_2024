ae18b5091716f962fdbd77778371e72b91fbbff6
= <=>
2024-06-21 13:33:01 +0200

    Final.

--- Robots/Wheeled_Chassis/Wheeled_Chassis_N64_Controller/Motor.ino
+++ Robots/Wheeled_Chassis/Wheeled_Chassis_N64_Controller/Motor.ino
@@ -1,3 +1,4 @@
+
 #include <N64Controller.h>
 #include <N64Interface.h>
 
@@ -8,18 +9,17 @@
 Servo s;
 int8_t deg;
 void update_servo() {
-	s.write(deg+45);
+        
+  	s.write(deg+45);
 }
  
 int8_t brzina;
  
- 
-int pinTasterLevo = 8;  
-int pinTasterDesno = 9;  
-int pinTasterNapred = 11;
-int pinTasterNazad = 12;
 int pwm_value ;
-int DIR = 4;
+int DIR = 4 ;
+int pinKontroler = 11;
+
+N64Controller controller(pinKontroler);
  
 void setup() {
 	Serial.begin(115200);
@@ -28,28 +28,25 @@ void setup() {
 	update_servo();
 	brzina = 0;
  
+        
  
  
  
- 
-  pinMode(pinTasterLevo, INPUT_PULLUP);  
-  pinMode(pinTasterDesno, INPUT_PULLUP);
-  pinMode(pinTasterNapred, INPUT_PULLUP);  
-  pinMode(pinTasterNazad, INPUT_PULLUP);
-  pinMode(DIR, OUTPUT);
+  pinMode(DIR , OUTPUT);
  
 }
  
 void loop() {
+
  
-  if(digitalRead(pinTasterLevo) == LOW && deg > -45) {
+  if(controller.D_left()) {
     deg -= DEG_STEP;
     Serial.print(deg);
     Serial.println("  skretanje");
     update_servo();
     delay(200);
   } 
-  if(digitalRead(pinTasterDesno) == LOW && deg < 45) {
+  if(controller.D_right()) {
     deg += DEG_STEP;
     Serial.print(deg);
     Serial.println("  skretanje");
@@ -58,17 +55,16 @@ void loop() {
   }
   
   if (deg > 45) {
-    brzina = 100;
-  } else if (brzina < -45) {
-    brzina = -45;
+    deg = 45;
+  } else if (deg < -45) {
+    deg = -45;
   }
   
-  
  
-  if (digitalRead(pinTasterNapred) == LOW && brzina < 100) {
+ if (controller.D_up()) {
     brzina += SPEED_STEP;
   } 
-  else if (digitalRead(pinTasterNazad) == LOW && brzina > -100) {
+  else if (controller.D_down()) {
     brzina -= SPEED_STEP;
   }
  
@@ -78,29 +74,29 @@ void loop() {
     brzina = -100;
   }
  
-  int pwm_value = map(brzina, -100, 100, 0, 255);
+  pwm_value = map(brzina, -100, 100, 0, 255);
  
  
   if ( brzina > 0 && brzina <= 100) {
     pwm_value = map(brzina, 100, 0, 0, 255);
     Serial.print(brzina);
-    Serial.println("  brzina");
-    analogWrite(3, pwm_value); 
+    Serial.println("Idem  napred");
     digitalWrite(DIR, HIGH );
+    analogWrite(3, pwm_value); 
   } else if(brzina < 0 && brzina >= -100){
     pwm_value = map(brzina, -100, 0 , 255, 0);
     Serial.print(brzina);
-    Serial.println("  brzina");
-    analogWrite(3, -pwm_value); 
+    Serial.println("  Idem nazad");
     digitalWrite(DIR , LOW );
+    analogWrite(3, -pwm_value);     
   }else if(brzina == 0){
     Serial.print(brzina);
     Serial.println("  brzina");
     analogWrite(3, 255);
   }
  
+ 
+   controller.update();
   // Pauza za stabilizaciju
   delay(200);
- 
- 
 }
